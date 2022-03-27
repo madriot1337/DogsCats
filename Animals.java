b9d2ee986bcc64800e6549a90c56c2ec55396a8e
madriot1337 <vbegeba22@mail.ru>
2022-03-27 10:24:35 +0300

    add DogsCats code

---
+++ Animals.java
@@ -0,0 +1,17 @@
+public class Animals {
+    int run;
+    int swim;
+    String name;
+
+
+    public Animals(){
+
+    }
+    public Animals( String n, int r, int s){
+        this.name = n;
+        this.run = r;
+        this.swim = s;
+
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat catBarsik = new Cat("Барсик",246, 56);
+        catBarsik.run();
+        catBarsik.swim();
+        Dog dogSharik = new Dog("Шарик",300,15);
+        dogSharik.run();
+        dogSharik.swim();
+
+    }
+}
